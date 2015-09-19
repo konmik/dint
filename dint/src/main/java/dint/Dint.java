c3161aa3fc12cdc6b025970ee8a5c290d0bde6bd
konmik <morelk@gmail.com>
2015-09-19 20:09:29 +0300

    1.0.0

---
+++ dint/src/main/java/dint/Dint.java
@@ -0,0 +1,200 @@
+package dint;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static java.lang.Math.min;
+
+/**
+ * Handling dates in a human-readable integer format (dint).
+ * Example: 20140912 = September 12, 2014
+ */
+public class Dint {
+
+    /**
+     * Creates a dint using given year, month and day.
+     */
+    public static int create(int year, int month, int day) {
+        return year * 10000 + month * 100 + day;
+    }
+
+    /**
+     * Creates a dint from Calendar.
+     */
+    public static int create(Calendar calendar) {
+        return compose(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    /**
+     * Creates a dint from Date.
+     */
+    public static int create(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return create(calendar);
+    }
+
+    /**
+     * Creates a dint using any number of years, months and days.
+     */
+    public static int compose(int year, int month, int day) {
+        return addDays(addMonths(year * 10000, month), day);
+    }
+
+    /**
+     * Returns a year of a given dint.
+     */
+    public static int year(int dint) {
+        return dint / 10000;
+    }
+
+    /**
+     * Returns a month of a given dint.
+     */
+    public static int month(int dint) {
+        return dint / 100 % 100;
+    }
+
+    /**
+     * Returns a day of month of in a given dint.
+     */
+    public static int day(int dint) {
+        return dint % 100;
+    }
+
+    /**
+     * Creates a Calendar instance from a given dint.
+     */
+    public static Calendar toCalendar(int dint) {
+        return new GregorianCalendar(year(dint), month(dint) - 1, day(dint));
+    }
+
+    /**
+     * Creates a Date instance from a given dint.
+     */
+    public static Date toDate(int dint) {
+        return toCalendar(dint).getTime();
+    }
+
+    /**
+     * Return the current date dint.
+     */
+    public static int today() {
+        return create(Calendar.getInstance());
+    }
+
+    /**
+     * Returns a dint at the first day of a month of a given dint.
+     */
+    public static int firstDayOfMonth(int dint) {
+        return compose(year(dint), month(dint), 1);
+    }
+
+    /**
+     * Returns a dint at the last day of a month of a given dint.
+     */
+    public static int lastDayOfMonth(int dint) {
+        return compose(year(dint), month(dint), daysInAMonth(year(dint), month(dint)));
+    }
+
+    /**
+     * Returns a number of days in a given month.
+     */
+    public static int daysInAMonth(int year, int month) {
+        return 31 - ((month == 2) ? (3 - (isLeapYear(year) ? 1 : 0)) : ((month - 1) % 7 % 2));
+    }
+
+    /**
+     * Returns if a given year is a leap year.
+     */
+    public static boolean isLeapYear(int year) {
+        return !((year % 4 != 0) || ((year % 100 == 0) && (year % 400 != 0)));
+    }
+
+    /**
+     * Returns amount of days between two given dints.
+     */
+    public static int diff(int dint1, int dint2) {
+        return toJulianDay(dint1) - toJulianDay(dint2);
+    }
+
+    /**
+     * Adds years to a dint. A negative number is allowed.
+     * The resulting dint's number of days will be limited by a number of days in the resulting month.
+     */
+    public static int addYears(int dint, int years) {
+        return composeLimit(dint, year(dint) + years, month(dint));
+    }
+
+    /**
+     * Adds years to a dint. A negative number is allowed.
+     * If the dint day is the last day in a month then the resulting day will be the last day in a month as well.
+     */
+    public static int addYearsExtend(int dint, int years) {
+        return composeExtend(dint, year(dint) + years, month(dint));
+    }
+
+    /**
+     * Adds months to a given dint. A negative number is allowed.
+     * The resulting dint's number of days will be limited by a number of days in the resulting month.
+     */
+    public static int addMonths(int dint, int add) {
+        int months = year(dint) * 12 + month(dint) + add;
+        return composeLimit(dint, months / 12, (months + 1) % 12 - 1);
+    }
+
+    /**
+     * Adds months to a given dint. A negative number is allowed.
+     * If the dint day is the last day in a month then the resulting day will be the last day in a month as well.
+     */
+    public static int addMonthsExtend(int dint, int add) {
+        int months = year(dint) * 12 + month(dint) + add;
+        return composeExtend(dint, months / 12, (months + 1) % 12 - 1);
+    }
+
+    /**
+     * Adds a given number of days to a given dint. A negative number is allowed.
+     */
+    public static int addDays(int dint, int days) {
+        return fromJulianDay(toJulianDay(dint) + days);
+    }
+
+    private static int composeLimit(int dint, int y, int m) {
+        return create(y, m, min(day(dint), daysInAMonth(y, m)));
+    }
+
+    private static int composeExtend(int dint, int y, int m) {
+        return daysInAMonth(year(dint), month(dint)) == day(dint) ?
+            create(y, m, daysInAMonth(y, m)) :
+            composeLimit(dint, y, m);
+    }
+
+    private static int toJulianDay(int dint) {
+        int year = year(dint);
+        int month = month(dint);
+        int day = day(dint);
+
+        int a = (14 - month) / 12;
+        int y = year + 4800 - a;
+        int m = month + 12 * a - 3;
+
+        return day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+    }
+
+    private static int fromJulianDay(int julianDay) {
+        int p = julianDay + 68569;
+        int q = 4 * p / 146097;
+        int r = p - (146097 * q + 3) / 4;
+        int s = 4000 * (r + 1) / 1461001;
+        int t = r - 1461 * s / 4 + 31;
+        int u = 80 * t / 2447;
+        int v = u / 11;
+
+        int Y = 100 * (q - 49) + s + v;
+        int M = u + 2 - 12 * v;
+        int D = t - 2447 * u / 80;
+
+        return Y * 10000 + M * 100 + D;
+    }
+}
---
+++ dint/src/test/java/dint/DintTest.java
@@ -0,0 +1,199 @@
+package dint;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static dint.Dint.addDays;
+import static dint.Dint.addMonths;
+import static dint.Dint.addMonthsExtend;
+import static dint.Dint.addYears;
+import static dint.Dint.addYearsExtend;
+import static dint.Dint.compose;
+import static dint.Dint.create;
+import static dint.Dint.daysInAMonth;
+import static dint.Dint.diff;
+import static dint.Dint.firstDayOfMonth;
+import static dint.Dint.isLeapYear;
+import static dint.Dint.lastDayOfMonth;
+import static org.junit.Assert.assertEquals;
+
+public class DintTest {
+
+    @Test
+    public void test_create_from_ymd() throws Exception {
+        assertEquals(20140912, create(2014, 9, 12));
+        assertEquals(0, create(0, 0, 0));
+        assertEquals(11332, create(1, 13, 32));
+    }
+
+    @Test
+    public void test_create_from_Calendar() throws Exception {
+        assertEquals(20140912, create(create20140912Calendar()));
+    }
+
+    @Test
+    public void test_create_from_Date() throws Exception {
+        assertEquals(20140912, create(new Date(2014 - 1900, 8, 12)));
+    }
+
+    @Test
+    public void test_compose() throws Exception {
+        assertEquals(20140912, compose(2014, 9, 12));
+        assertEquals(20141212, compose(2014, 12, 12));
+        assertEquals(20150112, compose(2014, 13, 12));
+        assertEquals(20140912, compose(2013, 9, 12 + 365));
+        assertEquals(20140912, compose(2015, 9, 12 - 365));
+        assertEquals(20150912, compose(2016, 9, 12 - 366));
+    }
+
+    @Test
+    public void test_year() throws Exception {
+        assertEquals(2014, Dint.year(20140912));
+    }
+
+    @Test
+    public void test_month() throws Exception {
+        assertEquals(9, Dint.month(20140912));
+    }
+
+    @Test
+    public void test_day() throws Exception {
+        assertEquals(12, Dint.day(20140912));
+    }
+
+    @Test
+    public void test_toCalendar() throws Exception {
+        assertEquals(create20140912Calendar(), Dint.toCalendar(20140912));
+    }
+
+    @Test
+    public void test_toDate() throws Exception {
+        assertEquals(create20140912Calendar().getTime(), Dint.toDate(20140912));
+    }
+
+    @Test
+    public void test_today() throws Exception {
+        assertEquals(createTodayCalendar(), Dint.toCalendar(Dint.today()));
+    }
+
+    @Test
+    public void test_firstDayOfMonth() throws Exception {
+        assertEquals(20140901, firstDayOfMonth(20140912));
+    }
+
+    @Test
+    public void test_lastDayOfMonth() throws Exception {
+        assertEquals(20140930, lastDayOfMonth(20140912));
+    }
+
+    @Test
+    public void test_daysInAMonth() throws Exception {
+        assertEquals(31, daysInAMonth(2014, 1));
+        assertEquals(28, daysInAMonth(2014, 2));
+        assertEquals(31, daysInAMonth(2014, 3));
+        assertEquals(30, daysInAMonth(2014, 4));
+        assertEquals(31, daysInAMonth(2014, 5));
+        assertEquals(30, daysInAMonth(2014, 6));
+        assertEquals(31, daysInAMonth(2014, 7));
+        assertEquals(31, daysInAMonth(2014, 8));
+        assertEquals(30, daysInAMonth(2014, 9));
+        assertEquals(31, daysInAMonth(2014, 10));
+        assertEquals(30, daysInAMonth(2014, 11));
+        assertEquals(31, daysInAMonth(2014, 12));
+        assertEquals(29, daysInAMonth(2016, 2));
+    }
+
+    @Test
+    public void test_isLeapYear() throws Exception {
+        assertEquals(false, isLeapYear(2014));
+        assertEquals(false, isLeapYear(2015));
+        assertEquals(true, isLeapYear(2016));
+        assertEquals(false, isLeapYear(2017));
+    }
+
+    @Test
+    public void test_diff() throws Exception {
+        assertEquals(365, diff(20140912, 20130912));
+        assertEquals(365, diff(20150912, 20140912));
+        assertEquals(366, diff(20160912, 20150912));
+
+        assertEquals(1, diff(20140912, 20140911));
+        assertEquals(-1, diff(20140912, 20140913));
+
+        assertEquals(31, diff(20140912, 20140812));
+    }
+
+    @Test
+    public void test_addYears() throws Exception {
+        assertEquals(20140912, addYears(20140912, 0));
+        assertEquals(20140912, addYears(20130912, 1));
+        assertEquals(20140912, addYears(20150912, -1));
+        assertEquals(20140912, addYears(10140912, 1000));
+
+        assertEquals(20160228, addYears(20140228, 2));
+    }
+
+    @Test
+    public void test_addYearsExtend() throws Exception {
+        assertEquals(20160229, addYearsExtend(20140228, 2));
+    }
+
+    @Test
+    public void test_addMonths() throws Exception {
+        assertEquals(20140912, addMonths(20140912, 0));
+        assertEquals(20140912, addMonths(20140812, 1));
+        assertEquals(20140912, addMonths(20141012, -1));
+        assertEquals(20140912, addMonths(20040912, 120));
+
+        assertEquals(20140912, addMonths(20130912, 12));
+        assertEquals(20140912, addMonths(20150912, -12));
+        assertEquals(20140912, addMonths(20240912, -120));
+
+        assertEquals(20140930, addMonths(20140831, 1));
+    }
+
+    @Test
+    public void test_addMonthsExtend() throws Exception {
+        assertEquals(20140912, addMonthsExtend(20140912, 0));
+        assertEquals(20140912, addMonthsExtend(20140812, 1));
+        assertEquals(20140912, addMonthsExtend(20140812, 1));
+        assertEquals(20140912, addMonthsExtend(20140812, 1));
+        assertEquals(20140912, addMonthsExtend(20140812, 1));
+        assertEquals(20141031, addMonthsExtend(20140930, 1));
+    }
+
+    @Test
+    public void test_days() throws Exception {
+        assertEquals(20140912, addDays(20140911, 1));
+        assertEquals(20140912, addDays(20140913, -1));
+        assertEquals(20140912, addDays(20150912, -365));
+        assertEquals(20140912, addDays(20141012, -30));
+    }
+
+    @Test
+    public void coverage_instantiate_utility_classes() throws Exception {
+        new Dint();
+    }
+
+    private Calendar create20140912Calendar() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.set(Calendar.YEAR, 2014);
+        calendar.set(Calendar.MONTH, 8);
+        calendar.set(Calendar.DAY_OF_MONTH, 12);
+        return calendar;
+    }
+
+    private Calendar createTodayCalendar() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        Calendar temp = new GregorianCalendar();
+        calendar.set(Calendar.YEAR, temp.get(Calendar.YEAR));
+        calendar.set(Calendar.MONTH, temp.get(Calendar.MONTH));
+        calendar.set(Calendar.DAY_OF_MONTH, temp.get(Calendar.DAY_OF_MONTH));
+        return calendar;
+    }
+}
